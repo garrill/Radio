504f5d90fe880cc5606ca83010ee84a9f12ca9f3
Jonny <jonny@garrill.com>
2026-03-04 15:48:28 +0000

    Initial Commit

---
+++ Radio/RadioApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadioApp.swift
+//  Radio
+//
+//  Created by Jonny Garrill on 04/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct RadioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RadioTests/RadioTests.swift
@@ -0,0 +1,16 @@
+//
+//  RadioTests.swift
+//  RadioTests
+//
+//  Created by Jonny Garrill on 04/03/2026.
+//
+
+import Testing
+
+struct RadioTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
